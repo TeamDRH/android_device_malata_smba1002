66e838d577edfc4d99da1ab291ff4d3b8cb26c17
wjb <jaybob413@gmail.com>
2012-02-15 12:41:05 -0500

    Use XOOM values for PRODUCT_BUILD_PROP_OVERRIDES

--- TeamDRH.mk
+++ TeamDRH.mk
@@ -6,8 +6,8 @@ PRODUCT_RELEASE_NAME := TeamDRH_smba1002
 # Inherit some common TeamDRH stuff.
 $(call inherit-product, vendor/TeamDRH/config/common_full_tablet_wifionly.mk)
 
-# Change these to values from a stock SMBA1002 rom
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=soju BUILD_ID=IML74K BUILD_FINGERPRINT=google/soju/crespo:4.0.3/IML74K/239410:user/release-keys PRIVATE_BUILD_DESC="soju-user 4.0.3 IML74K 239410 release-keys" BUILD_NUMBER=239410
+# Change these to values from a stock XOOM rom
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=tervigon BUILD_ID=IML77 BUILD_FINGERPRINT=motorola/tervigon/wingray:4.0.3/IML77/239789:user/release-keys PRIVATE_BUILD_DESC="tervigon-user 4.0.3 IML77 239789 release-keys" BUILD_NUMBER=239789
 
 # Additional AOSP Packages for TeamDRH build
 PRODUCT_PACKAGES += \
